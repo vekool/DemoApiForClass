44e4bb6940c0384f9c65a78e5b29864f92e3c146
Vivek <vivekrm@gmail.com>
2024-03-19 14:59:02 +0530

    Initial API Push

--- DemoApi/Controllers/EmployeeController.cs
+++ DemoApi/Controllers/EmployeeController.cs
@@ -7,9 +7,16 @@ namespace DemoApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+   /// <summary>
+   /// Employee Controller for CRUD operations on Employee table in DemoContext
+   /// </summary>
     public class EmployeeController : ControllerBase
     {
         readonly DemoContext dc;
+        /// <summary>
+        /// Constructor for injecting Democontext by the framework
+        /// </summary>
+        /// <param name="dc"></param>
         public EmployeeController(DemoContext dc)
         {
             this.dc = dc;
@@ -61,6 +68,77 @@ namespace DemoApi.Controllers
             dc.Employees.Add(e);
             await dc.SaveChangesAsync(); //201  Resource Created
             return CreatedAtAction(nameof(GetEmployee), new { id = e.EID }, e);
+        }
+
+        /// <summary>
+        ///  Delete an employee
+        /// </summary>
+        /// <param name="id"> The id of the employee </param>
+        /// <returns> The deleted employee</returns>
+        [HttpDelete]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Employee>> DeleteEmployee(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return NotFound("No Employee Id Specified");
+            }
+            Employee e = await dc.Employees.FindAsync(id);
+            if (e == null)
+            {
+                return NotFound("No Employee Found");
+            }
+            dc.Employees.Remove(e);
+            await dc.SaveChangesAsync();
+            return Ok(e);
+        }
+
+        /// <summary>
+        /// Update an employee
+        /// </summary>
+        /// <param name="id">The id to be edited</param>
+        /// <param name="e"> The employee data</param>
+        /// <returns> No Content</returns>
+
+        [HttpPut]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Employee>> UpdateEmployee(int? id, Employee e)
+        {
+            if (!id.HasValue)
+            {
+                return NotFound("No Employee Id Specified");
+            }
+            if(id.Value != e.EID)
+            {
+                return BadRequest("ID mismatch");
+            }
+            dc.Entry(e).State = EntityState.Modified;
+            //EF will automatcially generate the required update command
+            try
+            {
+                await dc.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!dc.Employees.Any(emp => e.EID == emp.EID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+            //200 range -- everything is ok
+            //400 - error
+            //500 - Server error
         }
     }
 }
